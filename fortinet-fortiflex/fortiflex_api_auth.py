787a6b0688cd74ca63d588c02eaeb76466e7fe86
Dylan Spille <dspille@fortinet.com>
2025-03-28 13:31:02 -0500

    trying to fix connector from timing out

--- fortinet-fortiflex/fortiflex_api_auth.py
+++ fortinet-fortiflex/fortiflex_api_auth.py
@@ -5,11 +5,13 @@ Copyright (c) 2023 Fortinet Inc
 Copyright end
 """
 
-import requests, json
-from time import time, ctime
+import json
 from datetime import datetime
-from connectors.core.utils import update_connnector_config
+from time import time, ctime
+
+import requests
 from connectors.core.connector import get_logger, ConnectorError
+from connectors.core.utils import update_connnector_config
 
 logger = get_logger('fortinet-fortiflex')
 
@@ -24,8 +26,11 @@ class FortiFlex:
         self.client_id = config.get("client_id")
         self.verify_ssl = config.get('verify_ssl')
         self.host = config.get("server")
+        # Initialize refresh_token from config if available
+        self.refresh_token = config.get("refreshToken")
+
         if self.host[:7] == "http://":
-            self.host = "https://{0}".format(self.host)
+            self.host = "https://{0}".format(self.host[7:])
         elif self.host[:8] == "https://":
             self.host = "{0}".format(self.host)
         else:
@@ -42,7 +47,13 @@ class FortiFlex:
             token_resp['expiresOn'] = (ts_now + token_resp['expires_in']) if token_resp.get("expires_in") else None
             token_resp['accessToken'] = token_resp.get("access_token")
             token_resp['refreshToken'] = token_resp.get("refresh_token")
-            token_resp.pop("access_token")
+
+            # Make sure to update the instance refresh_token for future refreshes
+            if token_resp.get("refresh_token"):
+                self.refresh_token = token_resp.get("refresh_token")
+
+            token_resp.pop("access_token", None)
+            token_resp.pop("refresh_token", None)
             return token_resp
         except Exception as err:
             logger.error("{0}".format(err))
@@ -54,20 +65,35 @@ class FortiFlex:
             if not connector_config.get('accessToken'):
                 logger.error('Error occurred while connecting server: Unauthorized')
                 raise ConnectorError('Error occurred while connecting server: Unauthorized')
+
             expires = connector_config['expiresOn']
             expires_ts = self.convert_ts_epoch(expires)
+
             if ts_now > float(expires_ts):
-                REFRESH_TOKEN_FLAG = True
-                self.refresh_token = connector_config['refreshToken']
                 logger.info("Token expired at {0}".format(expires))
-                token_resp = self.generate_token(REFRESH_TOKEN_FLAG)
+
+                # Make sure refresh_token is available
+                if not self.refresh_token and connector_config.get('refreshToken'):
+                    self.refresh_token = connector_config['refreshToken']
+
+                if not self.refresh_token:
+                    logger.error("No refresh token available for renewal")
+                    raise ConnectorError("No refresh token available. Please reconfigure the connector.")
+
+                token_resp = self.generate_token(True)  # Always pass True for REFRESH_TOKEN_FLAG when refreshing
+
                 connector_config['accessToken'] = token_resp['accessToken']
                 connector_config['expiresOn'] = token_resp['expiresOn']
+
                 if token_resp.get('refreshToken'):
-                    connector_config['refreshToken'] = token_resp.get('refreshToken')
-                update_connnector_config(connector_info['connector_name'], connector_info['connector_version'],
-                                         connector_config,
-                                         connector_config['config_id'])
+                    connector_config['refreshToken'] = token_resp['refreshToken']
+
+                update_connnector_config(
+                    connector_info['connector_name'],
+                    connector_info['connector_version'],
+                    connector_config,
+                    connector_config['config_id']
+                )
 
                 return "Bearer {0}".format(connector_config.get('accessToken'))
             else:
@@ -80,10 +106,10 @@ class FortiFlex:
 
 def acquire_token(self, REFRESH_TOKEN_FLAG):
     try:
-        error_msg = ''
         headers = {
             'Content-Type': 'application/json'
         }
+
         if not REFRESH_TOKEN_FLAG:
             data = {
                 "username": self.username,
@@ -92,50 +118,77 @@ def acquire_token(self, REFRESH_TOKEN_FLAG):
                 "grant_type": "password"
             }
         else:
+            if not self.refresh_token:
+                raise ConnectorError("No refresh token available")
+
             data = {
                 "grant_type": "refresh_token",
                 "refresh_token": self.refresh_token,
                 "client_id": self.client_id
             }
-        logger.debug("Payload: {0}".format(data))
+
+        logger.debug(
+            "Payload: {0}".format({k: '***' if k in ['password', 'refresh_token'] else v for k, v in data.items()}))
         endpoint = 'https://customerapiauth.fortinet.com/api/v1/oauth/token/'
         logger.debug("Endpoint: {0}".format(endpoint))
+
         response = requests.post(endpoint, data=json.dumps(data), headers=headers, verify=self.verify_ssl, timeout=60)
-        logger.debug("Response: {0}".format(response))
+        logger.debug("Response Status: {0}".format(response.status_code))
+
         if response.status_code in [200, 204, 201]:
             return response.json()
         else:
-            if response.text != "":
-                err_resp = response.json()
-                if err_resp and 'error' in err_resp:
-                    failure_msg = err_resp.get('error_description')
-                    error_msg = 'Response {0}: {1} \n Error Message: {2}'.format(response.status_code,
-                                                                                 response.reason,
-                                                                                 failure_msg if failure_msg else '')
-                else:
-                    err_resp = response.text
+            error_msg = ''
+            if response.text:
+                try:
+                    err_resp = response.json()
+                    if err_resp and 'error' in err_resp:
+                        failure_msg = err_resp.get('error_description', '')
+                        error_msg = 'Response {0}: {1} \n Error Message: {2}'.format(
+                            response.status_code, response.reason, failure_msg
+                        )
+                    else:
+                        error_msg = 'Response {0}: {1} \n Error Message: {2}'.format(
+                            response.status_code, response.reason, response.text
+                        )
+                except ValueError:
+                    error_msg = 'Response {0}: {1} \n Error Message: {2}'.format(
+                        response.status_code, response.reason, response.text
+                    )
             else:
-                error_msg = '{0}:{1}'.format(response.status_code, response.reason)
+                error_msg = 'Response {0}: {1}'.format(response.status_code, response.reason)
+
+            logger.error(error_msg)
             raise ConnectorError(error_msg)
 
+    except requests.exceptions.RequestException as e:
+        error_msg = f"Request error: {str(e)}"
+        logger.error(error_msg)
+        raise ConnectorError(error_msg)
     except Exception as err:
-        logger.error("{0}".format(str(err)))
+        error_msg = f"Unexpected error: {str(err)}"
+        logger.error(error_msg)
         raise ConnectorError(error_msg)
 
 
 def check(config, connector_info):
     try:
         co = FortiFlex(config)
-        if not 'accessToken' in config:
-            token_resp = co.generate_token(REFRESH_TOKEN_FLAG)
+        if 'accessToken' not in config:
+            token_resp = co.generate_token(False)  # Pass False for initial token generation
             config['accessToken'] = token_resp.get('accessToken')
             config['expiresOn'] = token_resp.get('expiresOn')
             config['refreshToken'] = token_resp.get('refreshToken')
-            update_connnector_config(connector_info['connector_name'], connector_info['connector_version'], config,
-                                     config['config_id'])
+            update_connnector_config(
+                connector_info['connector_name'],
+                connector_info['connector_version'],
+                config,
+                config['config_id']
+            )
             return True
         else:
-            token_resp = co.validate_token(config, connector_info)
+            co.validate_token(config, connector_info)
             return True
     except Exception as err:
+        logger.error(f"Health check failed: {str(err)}")
         raise ConnectorError(str(err))
